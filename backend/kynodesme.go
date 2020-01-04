2fa33ef1215b0e62af95d953eb3e638ab8e71acc
DragonBaby308 <2225072658@qq.com>
2020-01-04 09:55:15 +0800

    后端：hello world

---
+++ backend/kynodesme.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	if err := http.ListenAndServe("0.0.0.0:8082", nil); err != nil {
+		fmt.Println("HTTP监听端口失败！")	//改为日志 log
+	}
+}
+
+func Hello(w http.ResponseWriter, r * http.Request) {
+	//记录日志  log
+	fmt.Fprintf(w, "Hello World")
+}
